9653fd07ecfb378aee0408d0a94129683af71337
JustinCast <justincastillovalladares@gmail.com>
2020-05-06 11:55:16 -0600

    feat: added theme keys

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,138 @@
-export declare const helloWorld: () => string;
+export declare const EOSCR_THEME: {
+    colorScheme: {
+        primary: {
+            base: string;
+            800: string;
+            700: string;
+            600: string;
+            500: string;
+            400: string;
+            300: string;
+            200: string;
+            100: string;
+            50: string;
+            onPrimaryHighEmphasizedText: string;
+            onPrimaryMediumEmphasizedText: string;
+            onPrimaryDisabledText: string;
+            highEmphasizedBlackText: string;
+            mediumEmphasizedBlackText: string;
+            disabledBlackText: string;
+        };
+        secondary: {
+            base: string;
+            900: string;
+            800: string;
+            700: string;
+            600: string;
+            500: string;
+            400: string;
+            300: string;
+            200: string;
+            100: string;
+            50: string;
+            onSecondaryHighEmphasizedText: string;
+            onSecondaryMediumEmphasizedText: string;
+            onSecondaryDisabledText: string;
+            highEmphasizedWhiteText: string;
+            mediumEmphasizedWhiteText: string;
+            disabledWhiteText: string;
+        };
+    };
+    typography: {
+        fontFamilies: {
+            ptSans: string;
+            openSans: string;
+        };
+        h1: {
+            size: string;
+            letterSpacing: string;
+            fontWeight: string;
+            fontFamily: string;
+            lineHeight: string;
+        };
+        h2: {
+            fontSize: string;
+            letterSpacing: string;
+            fontWeight: string;
+            fontFamily: string;
+            lineHeight: string;
+        };
+        h3: {
+            fontSize: string;
+            letterSpacing: string;
+            fontWeight: string;
+            fontFamily: string;
+            lineHeight: string;
+        };
+        h4: {
+            fontSize: string;
+            letterSpacing: string;
+            fontWeight: string;
+            fontFamily: string;
+            lineHeight: string;
+        };
+        h5: {
+            fontSize: string;
+            letterSpacing: string;
+            fontWeight: string;
+            fontFamily: string;
+            lineHeight: string;
+        };
+        h6: {
+            fontSize: string;
+            letterSpacing: string;
+            fontWeight: string;
+            fontFamily: string;
+            lineHeight: string;
+        };
+        body1: {
+            fontSize: string;
+            letterSpacing: string;
+            fontWeight: string;
+            fontFamily: string;
+            lineHeight: string;
+        };
+        body2: {
+            fontSize: string;
+            letterSpacing: string;
+            fontWeight: string;
+            fontFamily: string;
+            lineHeight: string;
+        };
+        subtitle1: {
+            fontSize: string;
+            letterSpacing: string;
+            fontWeight: string;
+            fontFamily: string;
+            lineHeight: string;
+        };
+        subtitle2: {
+            fontSize: string;
+            letterSpacing: string;
+            fontWeight: number;
+            fontFamily: string;
+            lineHeight: string;
+        };
+        button: {
+            fontSize: string;
+            letterSpacing: string;
+            fontWeight: string;
+            fontFamily: string;
+            lineHeight: string;
+        };
+        caption: {
+            fontSize: string;
+            letterSpacing: string;
+            fontWeight: string;
+            fontFamily: string;
+            lineHeight: string;
+        };
+        overline: {
+            fontSize: string;
+            letterSpacing: string;
+            fontWeight: string;
+            fontFamily: string;
+            lineHeight: string;
+        };
+    };
+};
